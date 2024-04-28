3df5ee49d3b8f3c799fda24fec1e21dd88dfe1e7
pavelexpertov <pavelm01@hotmail.co.uk>
2024-04-28 11:27:20 +0100

    Amend mongodb version

--- set_up_scripts/vagrant_provisioning_scripts/set_up_mongodb.sh
+++ set_up_scripts/vagrant_provisioning_scripts/set_up_mongodb.sh
@@ -7,7 +7,7 @@ echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongod
 # Reload local package database
 sudo apt-get update
 # Installing a specific version of mongodb
-sudo apt-get install -y --allow-unauthenticated mongodb-org=3.6.23 mongodb-org-server=3.6.23 mongodb-org-shell=3.6.23 mongodb-org-mongos=3.6.23 mongodb-org-tools=3.6.23
+sudo apt-get install -y --allow-unauthenticated mongodb-org=3.6.2 mongodb-org-server=3.6.2 mongodb-org-shell=3.6.2 mongodb-org-mongos=3.6.2 mongodb-org-tools=3.6.2
 # Starting deamon
 sudo service mongod start
 
